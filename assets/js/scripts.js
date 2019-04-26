ccdb5a1ae89f08426825adf351475ec26ba32f8e
Natasha Fenech <natasha.fenech@corp.gaminginnovationgroup.com>
2019-04-26 15:09:57 +0200

    added prices

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,12 +17,17 @@ function scrollFunction() {
         $('#main-menu').removeClass('scrolled');
     }
 
-    $('section').each(function()
-    {
-        if ( $(this).offset().top < window.pageYOffset + 10  &&   $(this).offset().top +  $(this).height() > window.pageYOffset + 10) 
-        {
-          var data = $(this).data('id');
-          window.location.hash = data;
+    $('section').each(function () {
+        var data = $(this).data('id');
+
+        if ($(this).offset().top < window.pageYOffset + 10 && $(this).offset().top + $(this).height() > window.pageYOffset + 10) {
+            window.location.hash = data;
+        }
+
+        if ($(this).offset().top < window.pageYOffset + (window.innerHeight/2) && $(this).offset().top + $(this).height() > window.pageYOffset + (window.innerHeight/2)) {
+            if ($(this).hasClass('load-inner')) {
+                $(this).addClass('load');
+            }
         }
     });
 }
